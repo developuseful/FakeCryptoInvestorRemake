c537995c07153c794113b7600a0993d78dcc4249
Vyacheslav Pogudin <developuseful@gmail.com>
2023-04-21 00:33:34 +0800

    2

--- app/src/main/java/com/example/fakecryptoinvestorremake/domain/use_case/ProfitUpdateUseCase.kt
+++ app/src/main/java/com/example/fakecryptoinvestorremake/domain/use_case/ProfitUpdateUseCase.kt
@@ -1,45 +1,20 @@
 package com.example.fakecryptoinvestorremake.domain.use_case
 
-import android.util.Log
 import com.example.fakecryptoinvestorremake.data.remote.dto.toBitcoinPrice
-import com.example.fakecryptoinvestorremake.domain.models.Investment
 import com.example.fakecryptoinvestorremake.domain.repository.CoinRepository
 import com.example.fakecryptoinvestorremake.domain.repository.InvestmentRepository
-import kotlinx.coroutines.flow.onEach
 
 class ProfitUpdateUseCase(
     private val investmentRepository: InvestmentRepository,
     private val coinRepository: CoinRepository
 ) {
     suspend operator fun invoke() {
-
-        Log.d("ProfitUpdateUseCase", "1111")
-        val bitcoinPrice = coinRepository.getCoins()[0].toBitcoinPrice().price
-
-        val list = investmentRepository.getInvestmentsList()
-
-        Log.d("ProfitUpdateUseCase", list.toString())
-
-        Log.d("ProfitUpdateUseCase", "2222")
-
-
-        for (inv in list) {
-            investmentRepository.insertInvestment(
-                Investment(
-                    id = inv.id,
-                    name = inv.name,
-                    value = inv.value,
-                    hypothesis = inv.hypothesis,
-                    dateOfCreation = inv.dateOfCreation,
-                    profit = 100 - (inv.value * 100 / bitcoinPrice)
+        coinRepository.getCoins()[0].toBitcoinPrice().price.let { bitcoinPrice ->
+            investmentRepository.getInvestmentsList().onEach { investment ->
+                investmentRepository.insertInvestment(
+                    investment.copy(profit = 100 - (investment.value * 100 / bitcoinPrice))
                 )
-            )
-            Log.d("ProfitUpdateUseCase", inv.toString())
+            }
         }
-
-
-
-        Log.d("ProfitUpdateUseCase", "9999")
-
     }
 }
